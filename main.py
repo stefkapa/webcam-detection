f7b7b3aa7cd136747215fc58b96d1665866deee3
stef <giorgoskapaa@gmail.com>
2024-11-14 13:42:06 +0200

    send email when obj is in screen

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from curses.textpad import rectangle
+from emailing import send_email
+
 import cv2
 import time
 
@@ -5,8 +8,9 @@ video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
-
+status_list = []
 while True:
+    status = 0
     check, frame = video.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
@@ -26,7 +30,18 @@ while True:
         if cv2.contourArea(contour) < 5000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        if rectangle.any():
+            status = 1
+
+
+    status_list.append(status)
+    status_list = status_list[-2:]
+
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_email()
+
+    print(status_list)
 
     cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
