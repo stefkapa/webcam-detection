f79e6b1c047d6642d1e8bb2607b16fc2031378a5
stef <giorgoskapaa@gmail.com>
2024-11-15 21:15:24 +0200

    complete program

--- emailing.py
+++ emailing.py
@@ -1,2 +1,29 @@
-def send_email():
-    print("email sent")
+import imghdr
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+sender = "giorgoskapaa@gmail.com"
+password = "bhmtlkugpvaayswf"
+receiver = "giorgoskapaa@gmail.com"
+
+def send_email(image_path):
+    print("start")
+    email_message = EmailMessage()
+    email_message["Subject"] = "New customer showed up"
+    email_message.set_content("hey we just saw a new customer")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(sender, password)
+    gmail.sendmail(sender, receiver, email_message.as_string())
+    gmail.quit()
+    print("complete")
+
+if __name__ == "__main__":
+    send_email(image_path="images/19.png")
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import glob
+import os
+from threading import Thread
 from emailing import send_email
 import cv2
 import time
@@ -9,6 +11,13 @@ time.sleep(1)
 first_frame = None
 status_list = []
 count = 1
+
+def clean_folder():
+    print("start")
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+    print("complete")
 
 while True:
     status = 0
@@ -46,7 +55,15 @@ while True:
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        emai_thread = Thread(target=send_email, args=(image_with_objet, ))
+        emai_thread.daemon = True
+        clean_thread = Thread(target=clean_folder,)
+        clean_thread.daemon = True
+
+        emai_thread.start()
+        clean_thread.start()
+
+
 
     print(status_list)
 
